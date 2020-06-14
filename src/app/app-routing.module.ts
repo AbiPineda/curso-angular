76f79a181fea61e6cb5b35faa3c25f9717426fa3
AbiPineda <pineverdi@gmail.com>
2020-06-14 17:45:13 -0600

    contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 import { SearchComponent } from './pages/search/search.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 
 const app_routes: Routes = [
@@ -12,6 +13,7 @@ const app_routes: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'item/:id', component: ItemComponent },
     { path: 'search/:termino', component: SearchComponent },
+    { path: 'contacto', component: ContactoComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
